e040721272c30a7409babb83fd40b11d424baddd
Andriikryl <andreyk557@gmail.com>
2024-04-05 23:38:35 +0300

    add random page

---
+++ src/app/random/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/container/Container";
+import React from "react";
+
+export default function Random() {
+  return (
+    <section>
+      <Container>
+        <div>dsa</div>
+      </Container>
+    </section>
+  );
+}

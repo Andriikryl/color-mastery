127eadbdca47da00e607ee6b804315c85f458184
Andriikryl <andreyk557@gmail.com>
2024-04-06 20:08:54 +0300

    add title for hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,6 +4,7 @@ import { range } from "@/utils/range";
 import styles from "./style.module.css";
 import { motion } from "framer-motion";
 import { Nav } from "../nav/Nav";
+import MotionScene from "../motionScene/MotionScene";
 
 const colors = ["#d62828", "#ffbe0b", "#ffb3c6", "#c8b6ff", "#80ed99"];
 
@@ -30,7 +31,11 @@ export function Hero() {
           );
         })}
       </div>
-      <Nav />
+      <div className={styles.flex__group}>
+        <h1 className={styles.title}>Colors</h1>
+        <MotionScene />
+        <Nav />
+      </div>
     </section>
   );
 }

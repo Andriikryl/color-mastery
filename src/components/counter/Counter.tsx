5a5bcbde2ac05d5d747752524abc5244f3162d66
Andriikryl <andreyk557@gmail.com>
2024-04-06 18:20:14 +0300

    add Counter component

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { MotionValue, motion, useSpring, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+import styles from "./style.module.css";
+
+const fontSize = 30;
+const padding = 15;
+const height = fontSize + padding;
+
+export function Counter({ value }: { value: number }) {
+  return (
+    <div style={{ fontSize }} className={styles.wrapper}>
+      <Digit place={100} value={value} />
+      <Digit place={10} value={value} />
+      <Digit place={1} value={value} />
+    </div>
+  );
+}
+
+function Digit({ place, value }: { place: number; value: number }) {
+  let valueRoundedToPlace = Math.floor(value / place);
+  let animatedValue = useSpring(valueRoundedToPlace);
+
+  useEffect(() => {
+    animatedValue.set(valueRoundedToPlace);
+  }, [animatedValue, valueRoundedToPlace]);
+
+  return (
+    <div style={{ height }} className={styles.digits__box}>
+      {[...Array(10).keys()].map((i) => (
+        <Number key={i} mv={animatedValue} number={i} />
+      ))}
+    </div>
+  );
+}
+
+function Number({ mv, number }: { mv: MotionValue; number: number }) {
+  let y = useTransform(mv, (latest) => {
+    let placeValue = latest % 10;
+    let offset = (10 + number - placeValue) % 10;
+
+    let memo = offset * height;
+
+    if (offset > 5) {
+      memo -= 10 * height;
+    }
+
+    return memo;
+  });
+
+  return (
+    <motion.span style={{ y }} className={styles.number}>
+      {number}
+    </motion.span>
+  );
+}

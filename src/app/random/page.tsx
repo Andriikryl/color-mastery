3481ed165dfafc5cf8964bfe24f83768e6d5f9de
Andriikryl <andreyk557@gmail.com>
2024-04-06 18:22:52 +0300

    add random coloros for random page

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,61 +1,65 @@
 "use client";
-import React, { useEffect } from "react";
 import Container from "@/components/container/Container";
 import { getRandomHex } from "@/utils/randomHex";
 import { range } from "@/utils/range";
-import {
-  useMotionTemplate,
-  useMotionValue,
-  motion,
-  animate,
-} from "framer-motion";
 import styles from "./style.module.css";
-
-const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+import { useEffect, useState } from "react";
+import Plus from "@/icons/Plus";
+import VisuallyHidden from "@/components/VisuallyHidden/VisuallyHidden";
+import Minus from "@/icons/Minus";
+import { Counter } from "@/components/counter/Counter";
+import LinkBack from "@/components/linkBack/LinkBack";
 
 export default function Random() {
-  const color = useMotionValue(COLORS_TOP[0]);
+  const [amount, setAmount] = useState(16);
+  const [colors, setColors] = useState<string[]>([]);
   useEffect(() => {
-    animate(color, COLORS_TOP, {
-      ease: "easeInOut",
-      duration: 10,
-      repeat: Infinity,
-      repeatType: "mirror",
-    });
-  }, [color]);
-  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
-
+    const newColors = range(0, amount).map(() => getRandomHex());
+    setColors(newColors);
+  }, [amount]);
+  function Increment() {
+    setAmount(amount + 1);
+  }
+  function Dicrement() {
+    if (amount > 1) {
+      setAmount(amount - 1);
+    }
+  }
   return (
-    <motion.section
-      className={styles.random__section}
-      style={{
-        backgroundImage,
-      }}
-    >
+    <section className={styles.random__section}>
       <Container>
-        <div>
-          <h1>Random Color Generator</h1>
+        <div className={styles.group__box}>
+          <LinkBack />
+          <h1 className={styles.title}>Random Color Generator</h1>
+          <div className={styles.control__group}>
+            <button className={styles.button} onClick={Increment}>
+              <Plus />
+              <VisuallyHidden>Increment</VisuallyHidden>
+            </button>
+            <Counter value={amount} />
+            <button className={styles.button} onClick={Dicrement}>
+              <Minus />
+              <VisuallyHidden>decriment</VisuallyHidden>
+            </button>
+          </div>
           <div className={styles.palette__wrapper}>
-            {range(0, 10).map((item, index) => {
-              let randomColor = getRandomHex();
-              return (
-                <div key={index}>
-                  <div
-                    className={styles.palette__box}
-                    style={{ backgroundColor: `${randomColor}` }}
-                  ></div>
-                  <div>
-                    <p>
-                      hex:
-                      {randomColor}
-                    </p>
-                  </div>
+            {colors.map((color, index) => (
+              <div className={styles.wrapper__box} key={index}>
+                <div
+                  className={styles.palette__box}
+                  style={{ backgroundColor: `${color}` }}
+                ></div>
+                <div className={styles.palette__values}>
+                  <p className={styles.hex__color}>
+                    hex:
+                    {color}
+                  </p>
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         </div>
       </Container>
-    </motion.section>
+    </section>
   );
 }
